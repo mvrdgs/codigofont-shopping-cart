2ca313779265531b8bbbeef1c6bcd8c4ed61b84d
Viegas <mvrdgs@gmail.com>
2021-12-20 16:08:27 -0300

    feat: login page

---
+++ front-end/pages/login.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Flex, FormLabel, Input, Link } from '@chakra-ui/react';
+
+function Login() {
+  const [loginData, setLoginData] = useState({ email: '', password: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(loginData);
+  }
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    const updatedData = { ...loginData };
+    updatedData[name] = value;
+    setLoginData(updatedData);
+  }
+
+  return (
+    <Flex alignItems="center" justifyContent="center" height="100vh" bgColor="blue.400">
+      <Flex direction="column" shadow="2xl" rounded="10" p="10" bgColor="white">
+        <form onSubmit={ handleSubmit }>
+          <FormLabel mb="6">
+            Email
+            <Input
+              name="email"
+              type="text"
+              onChange={ handleChange }
+              autoComplete="off"
+            />
+          </FormLabel>
+          <FormLabel mb="6">
+            Senha
+            <Input
+              name="password"
+              type="password"
+              onChange={ handleChange }
+            />
+          </FormLabel>
+          <Input
+            type="submit"
+            value="Login"
+            mb="6"
+          />
+        </form>
+        <Link href="/register">Registre-se</Link>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Login;
