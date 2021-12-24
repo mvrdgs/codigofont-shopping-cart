0f0801c45a7037fdee969d9907583dce24d9c9b7
Viegas <mvrdgs@gmail.com>
2021-12-24 09:27:13 -0300

    feat: return image url instead of filename

--- back-end/src/controllers/products.js
+++ back-end/src/controllers/products.js
@@ -2,7 +2,7 @@ const uploadImage = (req, res, next) => {
   try {
     const { filename } = req.file;
 
-    return res.status(200).json({ filename })
+    return res.status(200).json({ fileUrl: `/image/${filename}` });
   } catch (error) {
     next(error);
   }
